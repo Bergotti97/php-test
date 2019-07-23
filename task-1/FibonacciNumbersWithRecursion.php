cadd21bb46bb6386e6d94df9da3672ca6fd1b366
Bergotti <grishmg97@gmail.com>
2019-07-23 11:52:43 +0300

    adding recursion

---
+++ task-1/FibonacciNumbersWithRecursion.php
@@ -0,0 +1,19 @@
+<?php
+
+function fibonacci( $n ) 
+{
+    if ($n == 0 ) {
+        return 0;
+    } elseif ($n == 1) {
+        return 1;
+    }
+    $result = (fibonacci($n-2) + fibonacci($n-1) );
+    return $result;
+}
+
+// Рекомендуется уменьшить количество чисел Фибоначи желательно до 30 так,
+// как для подсчета дальнейших чисел требуется огромное количество времени. 
+$n = 64;
+for ($i = 0; $i <= $n; $i++) { 
+    echo fibonacci($i)."\n";
+}
