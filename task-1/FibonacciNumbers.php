97baf050c1d963aa9bcfb3b9744b469b0cdc115b
Bergotti <grishmg97@gmail.com>
2019-07-23 11:52:56 +0300

    refactor

--- task-1/FibonacciNumbers.php
+++ task-1/FibonacciNumbers.php
@@ -1,9 +1,12 @@
 <?php
 
-$a = 0;
-$b = 1;
-$c = 1;
+$n = 64;
+$fibonacci[0] = 0;
+$fibonacci[1] = 1;
+echo "Fibonacci Number 0 is ". $fibonacci[0]."\n";
+echo "Fibonacci Number 1 is ". $fibonacci[1]."\n";
 
-for ($i = 1; $i <= 64; $i++, $c = $a + $b, $a = $b, $b = $c) {
-    echo $c.' ';
-}
+for ($i = 2; $i <= $n; $i++) { 
+    $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2]; 
+    echo "Fibonacci Number ".$i ." is ". $fibonacci[$i]."\n";
+} 
